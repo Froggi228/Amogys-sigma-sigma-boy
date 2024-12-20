255278b987f9150ca057dbb80f267b4e153a0f6f
Froggi228 <froggigamesroblox@gmail.com>
2024-12-20 11:18:18 +0500

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     //Вывести в консоль приветствия мира
+     print("HELLO WORLD!") 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+  ыыыы
+    }
+}
